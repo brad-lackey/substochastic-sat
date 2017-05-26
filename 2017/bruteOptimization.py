eef4f4d896659b7dc38c73cabae1f45acd60f551
Bryan Luu <bryanluu30794@gmail.com>
2017-05-26 18:02:48 -0400

    Implemented dynamic timeouts. Improved the reading/writing to the PROGRESS and RESULTS files. Added more output messages.

--- 2017/bruteOptimization.py
+++ 2017/bruteOptimization.py
@@ -7,7 +7,7 @@ from joblib import Parallel, delayed
 from itertools import product
 from cleanupBrute import cleanup
 import sys
-from multiprocessing import Queue
+import time
 
 if __name__ == "__main__":
 
@@ -32,9 +32,12 @@ if __name__ == "__main__":
 
     dT = np.ones(bins)
 
-    def saveProgress(progfile, index):
+    def saveProgress(progfile, index, success=True):
         with open(progfile, 'a') as f:
-            f.write("Job {0}/{1} Done\n".format(index + 1, len(A_list)))
+            if success:
+                f.write("Job {0}/{1} Done\n".format(index + 1, len(A_list)))
+            else:
+                f.write("Job {0}/{1} Timed Out!\n".format(index+1, len(A_list)))
 
     def loadProgress(progfile):
         indices = []
@@ -83,6 +86,7 @@ if __name__ == "__main__":
     # file to store the top MAX_LUT results
     resFile = tag + ".RESULTS.txt"
 
+    CUTOFF_TIME = 60
 
     """Returns a dictionary of the MAX_LUT best loops"""
     def getResults(lock):
@@ -94,12 +98,13 @@ if __name__ == "__main__":
             with open(resFile, 'r') as f:
                 line = f.readline()
                 while(len(line) > 0):
-                    iStr, loopStr, Astr = line.split(";")
+                    iStr, loopStr, tStr, Astr = line.split(";")
                     i = int(iStr.split('=')[1])
                     loop = float(loopStr.split('=')[1])
+                    t = float(tStr.rstrip('s').split('=')[1])
 
                     # save the loop from the file
-                    results[i] = loop
+                    results[i] = (loop, t)
 
                     line = f.readline()
         except IOError:
@@ -111,7 +116,7 @@ if __name__ == "__main__":
 
 
     """Returns a dictionary of the MAX_LUT best loops"""
-    def updateResults(index, loops, lock):
+    def updateResults(index, loops, timeout, lock):
         results = {}
 
         lock.lock()
@@ -121,21 +126,22 @@ if __name__ == "__main__":
             with open(resFile, 'r') as f:
                 line = f.readline()
                 while(len(line) > 0):
-                    tup = line.split("; ")
-                    if len(tup) != 3:
+                    tup = line.split(";")
+                    if len(tup) != 4:
                         line = f.readline()
                         continue
 
-                    iStr, loopStr, Astr = tup
+                    iStr, loopStr, tStr, Astr = tup
                     i = int(iStr.split('=')[1])
                     loop = float(loopStr.split('=')[1])
+                    t = float(tStr.rstrip('s').split('=')[1])
 
                     if i == index:
                         line = f.readline()  # skip if i is given index
                         continue
 
                     # save the loop from the file
-                    results[i] = loop
+                    results[i] = (loop, t)
 
                     if not new_min_found and loops < loop:
                         new_min_found = True
@@ -147,15 +153,24 @@ if __name__ == "__main__":
         if len(results) < MAX_LUT and index not in results:
             new_min_found = True
 
-        results[index] = loops
+        results[index] = (loops, timeout)
 
         # only write to results if there is reason to
         if new_min_found:
             with open(resFile, 'w') as f:
-                # iterate through results in sorted order by loop time, keeping only the top MAX_LUT
-                for i, loop in sorted(results.iteritems(), key=lambda x: x[1])[0:MAX_LUT]:
-                    f.write("index={0}; loops={1}; A={2}\n".format(i, loop, A_list[i]))
-            print("New minimum ({0}) added, at A={1}. See ".format(loops, A_list[index]) + resFile + " for results.")
+                # iterate through results in sorted order by loop time, keeping only the top MAX_LUT results
+                for i, tup in sorted(results.iteritems(), key=lambda x: x[1][0])[0:MAX_LUT]:
+                    loop, t = tup
+                    f.write("index={0}; loops={1}; time={2}s; A={3}\n".format(i, loop, t, A_list[i]))
+            print("New minimum ({0}) added, at A={1}. See ".format(loops, A_list[index]) + resFile + " for details.")
+
+        # set CUTOFF_TIME to the largest timeout in the top MAX_LUT results
+        lastVal = sorted(results.values(), key=lambda x: x[1], reverse=True)[0][1]
+
+        global CUTOFF_TIME
+        if lastVal < CUTOFF_TIME:
+            CUTOFF_TIME = lastVal
+            print("Using new timeout of {0}".format(CUTOFF_TIME))
 
         lock.unlock()
 
@@ -178,10 +193,14 @@ if __name__ == "__main__":
         args.append(fulltag)
 
         # returns 0 if successful otherwise throws error
+        begin = time.time()
         try:
-            check_call(args, timeout=210)
+            check_call(args, timeout=CUTOFF_TIME)
         except TimeoutExpired:
+            print("Job {0}/{1} Timed Out!".format(index+1, len(A_list)))
+            saveProgress(progfile, index, success=False)  # save the index so that we don't have to redo it
             return LOOP_PENALTY
+        timeout = time.time() - begin
 
         txtfile = fulltag + ".txt"
         hits, loops = parseTXT(txtfile)
@@ -193,7 +212,9 @@ if __name__ == "__main__":
 
         saveProgress(progfile, index)
 
-        results = updateResults(index, loops, queue)
+        updateResults(index, loops, timeout, queue)
+
+        print("Job {0}/{1} Done".format(index+1, len(A_list)))
 
         return loops
 
@@ -205,19 +226,15 @@ if __name__ == "__main__":
         # Print and save the best A's
         results = getResults(reslock)
 
-        for rank, tup in enumerate(sorted(results.iteritems(), key=lambda x: x[1])):
+        for rank, tup in enumerate(sorted(results.iteritems(), key=lambda x: x[1][0])):
             i = tup[0]
-            loop = tup[1]
-            print("Rank {0}: loops={1}, A={2}".format(rank+1, loop, A_list[i]))
+            loop, t = tup[1]
+            print("Rank {0}: loops={1}, A={2}, time={3}".format(rank+1, loop, A_list[i], t))
             makeLUT(tag + ".LUT.{0}.BEST.{1}.txt".format(bins, rank+1), bins, dT, A_list[i])
 
     except KeyboardInterrupt:
 
         print("Cleaning output files...")
-        datfile = sys.argv[1]
-
-        # construct tag from datfile title
-        tag = datfile.split('/')[-1].rstrip(".dat")
 
         # Cleans every output file up
         res = Parallel(n_jobs=-1)(delayed(cleanup)("{0}.{1}".format(tag, i), bins) for i, A in enumerate(A_list))
