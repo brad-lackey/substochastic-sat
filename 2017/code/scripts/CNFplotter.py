acfa9b14dd6bedc14bd1042681f424ba25723cbf
Bryan Luu <bryanluu30794@gmail.com>
2017-07-21 15:16:27 -0400

    Implemented more code to run tests for benchmarking the WCNF.

--- 2017/code/scripts/CNFplotter.py
+++ 2017/code/scripts/CNFplotter.py
@@ -3,63 +3,87 @@ import sys
 import numpy as np
 from subprocess import check_output, check_call
 import matplotlib.pyplot as plt
+import time as TIME
 
-def parseOutput(outStr):
+def parseOutput(outStr, program):
     lines = outStr.split('\n')
 
-    for line in lines:
-        if line.startswith("c Walltime:"):
-            time = float(line.split()[2])
+    time = -1
+    if program == "ssmc":
+        for line in lines:
+            if line.startswith("c Walltime:"):
+                time = float(line.split()[2])
 
-        elif line.endswith("sys"):
-            time = float(line.split()[0])
+            elif line.endswith("sys"):
+                time = float(line.split()[0])
+    elif program == "CCEHC":
+        for line in lines:
+            if line.startswith("c time"):
+                time = float(line.split()[4])
 
     return time
 
 
 def timeProgram(program, vars):
 
+    # create WCNF for the given number of variables
+
     args = []
 
     args.append("./generateCNF.py")
     args.append(str(vars))
-    args.append(str(1))
-    args.append(str(44))
-    args.append(str(-100))
+    args.append(str(8))     # qubits per cluster
+    args.append(str(100))   # J = 100
+    args.append(str(44))    # h1 = 44
+    args.append(str(-100))  # h2 = -100
     args.append("out.wcnf")
 
     check_call(args)
 
+    # solve the WCNF
+
     args = []
 
-    args.append("time")
     args.append(program)
     args.append("out.wcnf")
 
+    if program == "CCEHC":
+        args.append(TIME.time())
+        args.append("10")           # 10 seconds of runtime
+
     try:
         outStr = check_output(args)
-        return parseOutput(outStr)
+        return parseOutput(outStr, program)
 
     except Exception:
-        pass
+        print("Could not run \"{0}\"!".format(program))
+        return None
+
+def printResultsToCSV(csv_file, times, N):
+
+    with open(csv_file, 'w') as f:
+        f.write("Size (Qubits)," + ",".join(N) + "\n")
+        for pgm in times.keys():
+            f.write(pgm + "," + ",".join(times[pgm] + "\n"))
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
-        print("Usage: ./CNFplotter.py command")
+        print("Usage: ./CNFplotter.py <results.csv>")
         sys.exit(1)
 
-    program = sys.argv[1]
+    csv_file = sys.argv[1]
 
-    N = (np.arange(10) + 1) * 100
+    programs = []
 
-    times = [timeProgram(program, n) for n in N]
+    programs.append("CCEHC")
 
-    plt.plot(N, times)
-    plt.title("Times vs. Problem-size")
-    plt.xlabel("Problem size N")
-    plt.ylabel("Time in seconds to completion")
-    plt.show()
+    N = [39, 64, 125]
 
-    print times, N
+    times = {}
+
+    for program in programs:
+        times[program] = [timeProgram(program, n) for n in N]
+
+    printResultsToCSV(csv_file, times, N)
